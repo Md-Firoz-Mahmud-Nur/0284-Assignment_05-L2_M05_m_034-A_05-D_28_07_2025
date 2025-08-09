da007d9c8bbebc2c05228ad099e43894a1caf4fd
firoz.nur <firoznur5@gmail.com>
2025-08-09 14:39:44 +0600

    Add : User Model

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,5 @@
 import { model, Schema } from "mongoose";
-import { IAuthProvider, IsActive, IUser, Role } from "./user.interface";
+import { IAuthProvider, IUser, Role } from "./user.interface";
 
 const authProviderSchema = new Schema<IAuthProvider>(
   {
@@ -17,17 +17,11 @@ const userSchema = new Schema<IUser>(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String },
-    role: { type: String, enum: Object.values(Role), default: Role.USER },
-    phone: { type: String },
+    role: { type: String, enum: Object.values(Role), default: Role.SENDER },
     picture: { type: String },
-    address: { type: String },
     isDeleted: { type: Boolean, default: false },
-    isActive: {
-      type: String,
-      enum: Object.values(IsActive),
-      default: IsActive.ACTIVE,
-    },
     isVerified: { type: Boolean, default: false },
+    isBlocked: { type: Boolean, default: false },
     auths: [authProviderSchema],
   },
   { timestamps: true, versionKey: false }
