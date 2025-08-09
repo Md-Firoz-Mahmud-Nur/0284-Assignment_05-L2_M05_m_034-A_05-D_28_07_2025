d9a05ee955efa89ff58a4180cd5285f3ba1e9d18
firoz.nur <firoznur5@gmail.com>
2025-08-09 13:46:32 +0600

    Add : Auth

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,90 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
+import AppError from "../../errorHelpers/AppError";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { setAuthCookie } from "../../utils/setCookie";
+import { authServices } from "./auth.service";
+
+const credentialsLogin = catchAsync(async (req: Request, res: Response) => {
+  const loginInfo = await authServices.credentialsLogin(req.body);
+
+  setAuthCookie(res, loginInfo);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "User Logged In Successfully",
+    data: loginInfo,
+  });
+});
+
+const getNewAccessToken = catchAsync(async (req: Request, res: Response) => {
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Refresh Token is required");
+  }
+
+  const tokenInfo = await authServices.getNewAccessToken(
+    refreshToken as string
+  );
+
+  setAuthCookie(res, tokenInfo);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Get New Access Token Successfully",
+    data: tokenInfo,
+  });
+});
+
+const logout = catchAsync(async (req: Request, res: Response) => {
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  });
+
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  });
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "User Logged Out Successfully",
+    data: null,
+  });
+});
+
+const resetPassword = catchAsync(async (req: Request, res: Response) => {
+  const decodedToken = req.user;
+  const password = req.body.password;
+
+  const updatePassword = req.body.updatePassword;
+
+  await authServices.resetPassword(
+    decodedToken as JwtPayload,
+    password,
+    updatePassword
+  );
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Password Changed Successfully",
+    data: null,
+  });
+});
+
+export const authControllers = {
+  credentialsLogin,
+  getNewAccessToken,
+  logout,
+  resetPassword,
+};
---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,82 @@
+import bcrypt from "bcryptjs";
+import httpStatus from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
+import { envVariables } from "../../config/env";
+import AppError from "../../errorHelpers/AppError";
+import {
+  createNewAccessTokenWithRefreshToken,
+  createUserTokens,
+} from "../../utils/userTokens";
+import { IUser } from "../user/user.interface";
+import { User } from "../user/user.model";
+
+const credentialsLogin = async (payload: Partial<IUser>) => {
+  const { email, password } = payload;
+
+  const isUserExist = await User.findOne({ email });
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User does not exist");
+  }
+
+  const isPasswordMatched = await bcrypt.compare(
+    password as string,
+    isUserExist.password as string
+  );
+
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password");
+  }
+
+  const userTokens = createUserTokens(isUserExist);
+
+  const userWithoutPassword = isUserExist.toObject();
+  delete userWithoutPassword.password;
+
+  return {
+    accessToken: userTokens.accessToken,
+    refreshToken: userTokens.refreshToken,
+    user: userWithoutPassword,
+  };
+};
+
+const getNewAccessToken = async (refreshToken: string) => {
+  const accessToken = await createNewAccessTokenWithRefreshToken(refreshToken);
+  return {
+    accessToken,
+  };
+};
+
+const resetPassword = async (
+  decodedToken: JwtPayload,
+  password: string,
+  updatePassword: string
+) => {
+  const user = await User.findById(decodedToken.userId);
+
+  if (!user) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User does not exist");
+  }
+
+  const isPasswordMatched = await bcrypt.compare(
+    password,
+    user.password as string
+  );
+
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.UNAUTHORIZED, "Incorrect Password");
+  }
+
+  user.password = await bcrypt.hash(
+    updatePassword as string,
+    Number(envVariables.BCRYPT_SALT_ROUND)
+  );
+
+  await user.save();
+};
+
+export const authServices = {
+  credentialsLogin,
+  getNewAccessToken,
+  resetPassword,
+};
