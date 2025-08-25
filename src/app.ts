2ecf947f7dbff52387e061e7a9969118b2f70c8e
firoz.nur <firoznur5@gmail.com>
2025-08-25 09:51:09 +0600

    Add : Cors Origin

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,12 @@ app.use(
 );
 app.set("trust proxy", 1);
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/api/v1", router);
